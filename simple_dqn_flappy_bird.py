2ebba843cfc90f242cc52209e6b27b076d374195
onionwyl <onionwyl@qq.com>
2019-03-05 14:13:26 +0800

    Fix random exploration in dqn

--- simple_dqn_flappy_bird.py
+++ simple_dqn_flappy_bird.py
@@ -39,7 +39,7 @@ class DqnBot(object):
         pass
     def move(self, state):
         if np.random.rand() < self.epsilon:
-            return np.random.randint(0, 1)
+            return np.random.randint(0, 2)
         else :
             return np.argmax(self.model.predict(state)[0])
 
